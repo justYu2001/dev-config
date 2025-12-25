04e9afdd4b3088981fcf06b12493d81b7744d7ba
YU Chen <49834964+justYu2001@users.noreply.github.com>
2025-12-25 14:37:27 +0800

    Add files via upload

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- Go to next error
+vim.keymap.set("n", "zn", vim.diagnostic.goto_next)
+
+-- Go to previous error
+vim.keymap.set("n", "zk", vim.diagnostic.goto_prev)
